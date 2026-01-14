a6634094f8114c52425890d980b98d6b4fb8db50
luispizzilopes <luisfelipe1203lf@gmail.com>
2026-01-14 20:43:57 -0300

    [PRG-003]: refatora o script .sql da consulta principal da task 1

--- desafio-tecnico-paradigma-task-1/query.sql
+++ desafio-tecnico-paradigma-task-1/query.sql
@@ -1,17 +1,14 @@
-WITH RankedEmployees AS (
-    SELECT 
-        t1.nome AS nome_departamento,
-        t0.nome AS nome_pessoa,
-        t0.salario AS salario,
-        ROW_NUMBER() OVER (PARTITION BY t1.nome ORDER BY t0.salario DESC, t0.nome) AS rn
-    FROM pessoa t0
-    INNER JOIN departamento t1 
-        ON t0.departamento_id = t1.id
-)
-SELECT 
-    nome_departamento,
-    nome_pessoa,
-    salario
-FROM RankedEmployees
-WHERE rn = 1
-ORDER BY nome_departamento;
+SELECT
+    d.nome AS nome_departamento,
+    p.nome AS nome_pessoa,
+    p.salario
+FROM departamento d
+CROSS APPLY (
+    SELECT TOP (1)
+        p.nome,
+        p.salario
+    FROM pessoa p
+    WHERE p.departamento_id = d.id
+    ORDER BY p.salario DESC, p.nome
+) p
+ORDER BY d.nome;
